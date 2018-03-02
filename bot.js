a0f716b5ad1ae0d527480c4ce834d57f2fa940a5
internetperson <mike_novellino4biz@aol.com>
2018-03-02 16:12:36 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,7 +1,5 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
-var fs = require('fs');
-var file = "logs.txt";
 
 client.on('ready', () => {
   console.log(`Welcome to NSABot! Coded by internetperson!`);
@@ -10,12 +8,8 @@ client.on('ready', () => {
 
 client.on('message', function(message) {
         if (message.channel.isPrivate) {
-                var dmlog = `(Private) ${message.author.username}: ${message.content}`;
-                fs.appendFile(file, dmlog + "\n");
                 console.log(`(Private) ${message.author.username}: ${message.content}`);
         } else {
-                var log = `(${message.guild.name} / ${message.channel.name}) ${message.author.username}: ${message.content}`;
-                fs.appendFile(file, log + "\n");
                 console.log(`(${message.guild.name} / ${message.channel.name}) ${message.author.username}: ${message.content}`);
         }
 });
